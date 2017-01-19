a819de59ebc75531caef6b40016487a12397d839
JasonMorgan <jmorgan@f9vs.com>
2017-01-19 06:58:35 -0800

    Minor change to pass the flatten string test.

--- src/convertfrom-yml.ps1
+++ src/convertfrom-yml.ps1
@@ -1,22 +1,25 @@
 
 function ConvertFrom-Yml {
-  [CmdletBinding()]
+  [CmdletBinding(DefaultParameterSetName='Default')]
   param (
     # Parameter help description
     [Parameter(Mandatory,
     ValueFromPipeline,
     ValueFromPipelineByPropertyName,
     ParameterSetName='Default')]
-    [string]$InputObject,
+    [string[]]$InputObject,
     [Parameter(ParameterSetName='File')]
     [string]$DestinationPath,
     [Parameter(Mandatory,
-    ValueFromPipeline,
-    ValueFromPipelineByPropertyName,
     ParameterSetName='File')]
     [string]$Path
   )
   begin {}
-  process {}
-  end {}
+  process {
+    $string += $InputObject
+  }
+  end {
+    $string = $string | Out-String
+    $string
+  }
 }
